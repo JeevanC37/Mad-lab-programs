1e07711bd67f8f27f9417e0a5af3ec6dbc513a89
Jeevan C <71091060+JeevanC37@users.noreply.github.com>
2025-06-08 19:47:30 +0530

    Add files via upload

---
+++ Lab3-ServiceWorker/sw.js
@@ -0,0 +1,35 @@
+var mycache = "mycache";
+
+var assests = [
+        	"/", 	
+        	"/my.html",
+        	"/img/bathroomprod.jpg",
+        	"/img/camera.jpg",
+        	"/img/headphone.jpeg",
+        	"/img/cosmetics.jpg",
+        	"/js/start.js",
+        	"/sw.js",
+        	“/manifest.json”
+];
+
+self.addEventListener('install', _event => {
+        	console.log('inside the install', _event);
+        	caches.open(mycache)
+        	.then(cache => {
+                    	cache.addAll(assests);
+        	});
+});
+
+self.addEventListener('activate', _event => {
+        	console.log('inside the activate', _event);
+});
+
+self.addEventListener('fetch', async (event) => {
+        	event.respondWith(
+                    	caches.match(event.request)
+                                	.then(respevt => {
+                                            	return respevt || fetch(event.request);
+                    	})
+        	);
+        	console.log('inside the fetched', _event);
+});
